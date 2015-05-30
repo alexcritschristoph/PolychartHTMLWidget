b59100b6b1f78dd374724740ea6bb57bcf20a465
Alex Crits-Christoph <acritsc1@jhu.edu>
2015-05-29 17:36:14 -0700

    initial commit.

---
+++ R/polychart.R
@@ -0,0 +1,56 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+polychart <- function(gg_obj, width = NULL, height = NULL, data_cols=c(0,1), palette=c("#3182bd","#fd8d3c","#74c476")) {
+
+  palette = as.factor(palette)
+
+  #Get data
+  gg_data = gg_obj$data
+  g_facet = toString(unlist(gg_obj$facet[1])$rows)
+  
+  title = gg_obj$labels['title'][[1]]
+  #Get color
+  g_color = gg_obj$labels['colour'][[1]]
+  
+  gg_data$color_val <- palette[as.factor(gg_data[[g_color]])]
+  
+  # forward options using x
+  x = list(
+    data = gg_data,
+    facet = g_facet,
+    color = g_color,
+    title = title,
+    x_var = gg_obj$labels['x'][[1]],
+    y_var = gg_obj$labels['y'][[1]],
+    data_cols = data_cols
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'polychart',
+    x,
+    width = width,
+    height = height,
+    package = 'polychart'
+  )
+}
+
+#' Widget output function for use in Shiny
+#'
+#' @export
+polychartOutput <- function(outputId, width = '100%', height = '400px'){
+  shinyWidgetOutput(outputId, 'polychart', width, height, package = 'polychart')
+}
+
+#' Widget render function for use in Shiny
+#'
+#' @export
+renderPolychart <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  shinyRenderWidget(expr, polychartOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/polychart.js
@@ -0,0 +1,73 @@
+HTMLWidgets.widget({
+
+  name: 'polychart',
+
+  type: 'output',
+
+  initialize: function(el, width, height) {
+
+    return {
+      // TODO: add instance fields as required
+    }
+
+  },
+
+  renderValue: function(el, x, instance) {
+    
+    //Create JSON data.
+    var searchterms = polyjs.data(x.data);
+    var x_r = Math.max.apply(null, x.data[x.x_var]) - Math.min.apply(null, x.data[x.x_var])
+    var x_min = Math.min.apply(null, x.data[x.x_var]) - 0.05*x_r;
+    var x_max = Math.max.apply(null, x.data[x.x_var]) + 0.05*x_r;
+    
+    var y_r = Math.max.apply(null, x.data[x.y_var]) - Math.min.apply(null, x.data[x.y_var])
+    var y_min = Math.min.apply(null, x.data[x.y_var]) - 0.05*y_r;
+    var y_max = Math.max.apply(null, x.data[x.y_var]) + 0.05*y_r;
+  //Assign properties.
+	polyjs.chart({
+	    layer: {
+	        data: searchterms,
+	        type: "point",
+	        x: x.x_var,
+	        y: x.y_var,
+          color: x.color,
+          tooltip: function(ex) { 
+            var html_string = "";
+            for (i = 0; i < x.data_cols.length; i++) { 
+              html_string = html_string + x.data_cols[i] + ": " + ex[x.data_cols[i]] + "\n"
+            }            
+            return html_string; 
+          }
+          },
+	     facet: {
+        type: "wrap",
+        var: x.facet,
+        formatter: function(facetObject) {
+            var title = x.facet + ": " + facetObject[x.facet];
+            return title;
+        	}
+        },
+        guides: {
+        y: {
+        min: y_min,
+        max: y_max,
+        },
+        x: {
+        min: x_min,
+        max: x_max,
+        }, 
+        
+        },
+	    title: x.title,
+	    dom: el.id,
+	    width: 600,
+	    height: el.height
+	});
+
+  },
+
+  resize: function(el, width, height, instance) {
+
+  }
+
+});
